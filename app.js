f9e31e15803b707c5a5118fced78f0f46f524e08
1612421 <nguyenngocnghia53@gmail.com>
2019-10-23 00:30:35 +0700

    fix cors

--- app.js
+++ app.js
@@ -37,6 +37,21 @@ app.use(flash());
 // Config passport
 require('./config/passport');
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+      'Access-Control-Allow-Headers', 
+      'Origin, X-Request-With, Accept, Authorization'
+  );
+
+  if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE');
+      return res.status(200).json({});
+  }
+
+  next();
+})
+
 app.use(getPayloadToken);
 app.use('/user', usersRouter);
 app.use('/', indexRouter);
