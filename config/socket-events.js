5cb4bf2a06715421ab3f9f9e691bc7fbf09cd1bd
1612421 <nguyenngocnghia53@gmail.com>
2019-11-05 23:06:38 +0700

    Thêm xử lí drawn, new game, undo (socketio)

--- config/socket-events.js
+++ config/socket-events.js
@@ -68,19 +68,21 @@ exports = module.exports = (io) => {
         // Xử lí khi rời khỏi phòng
         socket.on('leave room', () => {
             const room = rooms[socket.id];
-            socket.nsp.to(room).emit('game end');
-            const socketId = room.split('#');
-            allUsers[socketId[0]].leave(room);
-            allUsers[socketId[1]].leave(room);
-            delete rooms[socketId[0]];
-            delete rooms[socketId[1]];
-            delete names[socketId[0]];
-            delete names[socketId[1]];
-            delete avatars[socketId[0]];
-            delete avatars[socketId[1]];
-            delete allUsers[socketId[0]];
-            delete allUsers[socketId[1]];
+            socket.nsp.to(room).emit('game end', socket.Id);
 
+            if (room) {
+                const socketId = room.split('#');
+                allUsers[socketId[0]].leave(room);
+                allUsers[socketId[1]].leave(room);
+                delete rooms[socketId[0]];
+                delete rooms[socketId[1]];
+                delete names[socketId[0]];
+                delete names[socketId[1]];
+                delete avatars[socketId[0]];
+                delete avatars[socketId[1]];
+                delete allUsers[socketId[0]];
+                delete allUsers[socketId[1]];
+            }
         });
 
         // Xử lí khi mất kết nối
@@ -105,6 +107,79 @@ exports = module.exports = (io) => {
         socket.on('surrender', () => {
             const room = rooms[socket.id];
             socket.nsp.to(room).emit('surrender', socket.id);
+        });
+
+        // Xử lí khi user gửi yêu cầu xin hòa
+        socket.on('drawn', () => {
+            const room = rooms[socket.id];
+            socket.broadcast.to(room).emit('enemy suggest drawn');
+        });
+
+        // Xử lí khi user đồng ý hòa
+        socket.on('agree drawn', () => {
+            const room = rooms[socket.id];
+            socket.nsp.to(room).emit('drawn game');
+        });
+
+        // Xử lí khi user không đồng ý hòa
+        socket.on('disagree drawn', () => {
+            const room = rooms[socket.id];
+            socket.broadcast.to(room).emit('disagree drawn');
+        });
+
+        // Xử lí yêu cầu tạo game mới
+        socket.on('suggest new game', () => {
+            const room = rooms[socket.id];
+            socket.broadcast.to(room).emit('enemy suggest new game');
+        });
+
+        // Xử lí khi user đồng ý tạo game mới
+        socket.on('agree new game', () => {
+            const room = rooms[socket.id];
+            socket.nsp.to(room).emit('new game');
+        });
+
+        // Xử lí khi user từ chối tạo game mới
+        socket.on('reject new game', () => {
+            const room = rooms[socket.id];
+            socket.broadcast.to(room).emit('enemy reject new game');
+        });
+
+        // Xử lí khi user yêu cầu undo
+        socket.on('suggest undo', () => {
+            const room = rooms[socket.id];
+            socket.broadcast.to(room).emit('enemy suggest undo');
+        });
+
+        // Xử lí khi user chấp nhận undo
+        socket.on('agree undo', () => {
+            const room = rooms[socket.id];
+            socket.nsp.to(room).emit('allow undo');
+        });
+
+        // Xử lí khi user từ chối undo
+        socket.on('reject undo', () => {
+            const room = rooms[socket.id];
+            socket.broadcast.to(room).emit('enemy reject undo');
+        });
+
+        socket.on('disconnect', () => {
+            const room = rooms[socket.id];
+            socket.nsp.to(room).emit('game end', socket.Id);
+
+            if (room) {
+                const socketId = room.split('#');
+                allUsers[socketId[0]].leave(room);
+                allUsers[socketId[1]].leave(room);
+                delete rooms[socketId[0]];
+                delete rooms[socketId[1]];
+                delete names[socketId[0]];
+                delete names[socketId[1]];
+                delete avatars[socketId[0]];
+                delete avatars[socketId[1]];
+                delete allUsers[socketId[0]];
+                delete allUsers[socketId[1]];
+            }
         });
     });
 }
