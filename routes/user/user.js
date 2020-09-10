d5fe3667ebe1d9d21bfa3bb85417d2754b32745e
nghiann <nghiann@vng.com.vn>
2020-09-10 14:34:00 +0700

    fix cookie

--- routes/user/user.js
+++ routes/user/user.js
@@ -23,7 +23,7 @@ router.post('/register', notLogged, (req, res) => {
             }
 
             let token = jwt.generateJWT(user, process.env.SECRET_KEY, process.env.EXPIRE_IN);
-            res.cookie('Authorization', `Bearer ${token}`, {httpOnly: true});
+            res.cookie('Authorization', `Bearer ${token}`, {httpOnly: true, sameSite: "none"});
             return res.status(200).json({messages: ['register successfully']});
         });
     })(req, res)
